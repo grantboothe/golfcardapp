676a9e77c06c0e66802e3164f56a944fe7db93b4
grantboothe <grantboothe@gmail.com>
2016-06-16 11:17:58 -0600

    final project

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var numplayers = 8;
-var numholes = 18;
+var numholes = 19;
 var objmine;
 
 function addplayers() {
@@ -17,17 +17,14 @@ function deleteplayers() {
 };
 
 
-function buildcard() {
+function buildcard(teeboxid) {
     var holecollection = "";
     var playercollection = "";
     var totalscorecollection = "";
 
     for (var pl = 1; pl <= numplayers; pl++) {
-        playercollection += "<div id='player" + pl + "' class='holebox playerbox'> Player " + pl + "</div>";
-    }
+        playercollection += "<div id='player" + pl + "' class='holebox playerbox'> Player " + pl + "</div><div id='total" + pl+ "'>0</div>"; //
 
-    function setCourseInfo() {
-        buildcard();
     }
 
     for(var bob=18; bob >= 1; bob--){
@@ -40,7 +37,7 @@ function buildcard() {
                 title = 'Out';
         }
         if (title !== 'Out' || title !== 'Total'){
-            holecollection += "<div id='column" + bob + "' class='holecol'>"+ bob+"<div>par: "+ objmine.course.holes[(bob - 1)].tee_boxes[0].par+"<div>yards: " + objmine.course.holes[(bob - 1)].tee_boxes[0].yards+""+ "<div>hcp: "+objmine.course.holes[(bob - 1)].tee_boxes[0].hcp+"</div></div></div></div>";
+            holecollection += "<div id='column" + bob + "' class='holecol'>"+ bob+"<div>par: "+ objmine.course.holes[(bob - 1)].tee_boxes[teeboxid].par+"<div>yards: " + objmine.course.holes[(bob - 1)].tee_boxes[teeboxid].yards+""+ "<div>hcp: "+objmine.course.holes[(bob - 1)].tee_boxes[teeboxid].hcp+"</div></div></div></div>";
         }
         else {
             holecollection += "<div id='column" + bob + "' class='holecol'>"+ title+"</div>";
@@ -60,6 +57,9 @@ function buildholes() {
             $("#column" + h).append("<input onkeyup='scoretotal(" + p + ")' id='player" + p + "hole" + h + "' class='holebox'/>");
         }
     }
+    /*for(var z = 1; z <= numplayers; z++){
+        $("#column" + h).append("<input onkeyup='scoretotal(" + p + ")' id='player" + p + "total" + h + "' class='totalbox'/>");
+    }*/
 }
 
 
@@ -94,9 +94,16 @@ function selectCourse(id) {
 
         }
     };
+
+   /* for(var x = 0; x< myobj.courses[id].holes[0].tee_boxes.length; x++) {
+        $("#theteebox").append("<option value = '" + myobj.courses[id].holes[0].tee_boxes[x] + "'>" + myobj.courses[id].holes[0].tee_boxes[x].tee_type + "</option> ");
+    }*/
+    $("#theteebox").append("<option value = '0'>men </option>");
+    $("#theteebox").append("<option value = '1'>women </option>");
+    $("#theteebox").append("<option value = '2'>pro </option>");
     extragolf.open("GET", "http://golf-courses-api.herokuapp.com/courses/" + id , true);
     extragolf.send();
-    buildcard();
+    //buildcard();
 }
 var objmine = {};
 var golflist;
@@ -118,7 +125,6 @@ function populatelist(city) {
         if (golflist.readyState == 4 && golflist.status == 200) {
             myobj = JSON.parse(golflist.responseText);
             for (list = 0; list < myobj.courses.length; list++) {
-                console.log(myobj.courses[list].id);
                 $("#courseSelectionMenu").append("<option value = '" + myobj.courses[list].id + "'>" + myobj.courses[list].name + "</option> ");
             }
         }
@@ -145,11 +151,12 @@ function populatelist(city) {
 
 
 
-    function setCourseInfo(teeboxid){
-        buildcard(teeboxid);
-    }
+    
 }
 
+function setCourseInfo(teeboxid){
+    buildcard(teeboxid);
+}
 function Messages(){
     if (totalScore < par){
         $('#message').append('<div class="success" role="alert")>On to the PGA!</div>')
@@ -166,10 +173,15 @@ function Messages(){
 };
 
 
-function thescore(theplayer){
+function scoretotal(theplayer){
     var thetotal = 0;
-    for(var t = 1; t <= numholes; t++){
+    var lasthole = $("#player" + theplayer + "hole" + 18).val();
+    for(var t = 1; t < numholes; t++){
         thetotal += Number($("#player" + theplayer + "hole" + t).val());
     }
-    $("#grand" + theplayer).html(thetotal);
+    if(lasthole != ""){
+        alert("player " + theplayer + " score: " + thetotal);
+    }
+ //   $("#total" + theplayer).html(thetotal);
+    document.getElementById("total" + theplayer).innerHTML = thetotal;
 }
